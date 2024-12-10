0fd8bd692975f3bec90c5225e23f9ab3ff3e41c9
ChiaraNughedu <chianughedu97@gmail.com>
2024-12-10 15:21:53 +0100

    es2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 const nameInput = document.getElementById("nameInput");
 const savedNameDisplay = document.getElementById("savedName");
 const saveButton = document.getElementById("saveButton");
-const removeButton = document.getElementById("removeButton"); // Funzione per aggiornare la visualizzazione del nome salvato function updateSavedNameDisplay() { const savedName = localStorage.getItem('userName'); savedNameDisplay.textContent = savedName ? savedName : 'Nessun nome salvato.'; } // Carica il nome salvato quando la pagina viene caricata
+const removeButton = document.getElementById("removeButton"); 
+
+
+function updateSavedNameDisplay() { const savedName = localStorage.getItem('userName'); 
+    savedNameDisplay.textContent = savedName ? savedName : 'Nessun nome salvato.'; 
+} 
+
 window.onload = function () {
   updateSavedNameDisplay();
-}; // Salva il nome nel localStorage
+}; 
 saveButton.onclick = function () {
   const userName = nameInput.value;
   if (userName) {
@@ -12,10 +18,24 @@ saveButton.onclick = function () {
     updateSavedNameDisplay();
     nameInput.value = "";
   } else {
-    alert("Per favore, inserisci un nome.");
+    alert("Devi inserire un nome.");
   }
-}; // Rimuovi il nome dal localStorage
+}; 
+
 removeButton.onclick = function () {
   localStorage.removeItem("userName");
   updateSavedNameDisplay();
 };
+
+//secondo esercizio
+
+let seconds = sessionStorage.getItem("seconds")
+  ? parseInt(sessionStorage.getItem("seconds"))
+  : 0;
+const timeCounter = document.getElementById("timeCounter");
+timeCounter.textContent = seconds;
+setInterval(() => {
+  seconds++;
+  timeCounter.textContent = seconds;
+  sessionStorage.setItem("seconds", seconds);
+}, 1000);
